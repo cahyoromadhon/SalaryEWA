cb427cb62ba3c045876aefde7c9fa355666dab4f
cahyorom <gantengcahyo27@gmail.com>
2025-11-22 20:49:19 +0700

    Foundry Format

--- src/PhiiCoin.sol
+++ src/PhiiCoin.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-forge fmt --check
 
 contract PhiiCoin is ERC20 {
     constructor(uint256 initialSupply) ERC20("Phii Coin", "PHII") {
